a1841ff27bea8d90144389d1dc3567092168b765
SRTENEBROSO00 <ccastro@cincinnatus.edu.do>
2025-07-02 15:59:29 -0400

    Implement code changes to enhance functionality and improve performance

--- src/components/section/Project_section.tsx
+++ src/components/section/Project_section.tsx
@@ -1,30 +1,42 @@
-import { Github } from "lucide-react";
+import { Github, SquareArrowOutUpRight } from "lucide-react";
 import { Badge } from "@mui/material";
 
 import Image from "../../assets//noImageYet.webp"
+import Mikens from "../../../public/systemImagenMikens.png"
+
 
 
 function Project_section() {
   // Los datos dentro de los proyectos
   const projectData = [{
-    projectName: 'There is not projects yet',
-    image: Image,
-    decription: "I'll add all my future new projects soon"
-    // badge: ["React", "Tailwind", "Next.js", "VSCode"]
+    projectName: 'Mikens',
+    image: Mikens,
+    status: true,
+    github: "https://github.com/jeremyda173/sistem-fact-2",
+    linkProject: "https://sistem-fact-2.vercel.app/",
+    decription: "System to build systems",
+    badge: ["React", "Chakra", "Next.js", "VSCode"]
   },
   {
-    projectName: 'There is not projects yet',
+    projectName: 'Building one...',
     image: Image,
-    decription: "I'll add all my future new projects soon"
-    // badge: ["React", "Tailwind", "Next.js", "VSCode"]
+    status: false,
+    github: "",
+    linkProject: "",
+    decription: "I'll add all my future new projects soon",
+    badge: []
   },
   {
-    projectName: 'There is not projects yet',
+    projectName: 'Building one...',
     image: Image,
-    decription: "I'll add all my future new projects soon"
-    // badge: ["React", "Tailwind", "Next.js", "VSCode"]
+    status: false,
+    github: "",
+    linkProject: "",
+    decription: "I'll add all my future new projects soon",
+    badge: []
 
-  }]
+  }
+  ]
 
   return (
     <div id='projects' data-aos="fade-up" className=' w-screen'>
@@ -39,30 +51,61 @@ function Project_section() {
           </div>
 
           {/* Projects Grid */}
-          <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-8  '>
+          <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-8 '>
             {/* Render */}
             {projectData.map((projectGroup, index) => (
-              <div key={index} className=' group h-96 flex flex-col justify-around items-start bg-black/40 backdrop-blur-sm border border-white/10 rounded-xl overflow-hidden hover:border-orange-500/50 transition-all hover:shadow-orange-500/10 hover:shadow-lg p-5 '>
-                <div className='relative'>
-                  <div className=' absolute inset-0 bg-gradient-to-t from-black/80 to-transparent opacity-0 group-hover:opacity-100 h-full transition-opacity flex items-end justify-between p-4'>
-                    <a href="">
-                      <Github className='hover:text-white hover:bg-amber-700 hover:rounded-2xl pointer-events-auto w-20 h-6'  />
-                    </a>
+              <div key={index} className=' group h-96 flex flex-col justify-around items-start bg-black/40 backdrop-blur-sm border border-white/10 rounded-xl overflow-hidden hover:border-orange-500/50 transition-all hover:shadow-orange-500/10 hover:shadow-lg p-5 hover:animate-pulse cursor-pointer'>
+                <div className='relative w-full h-40 flex items-center justify-center overflow-hidden rounded-lg bg-gray-900'>
+                  <div className='absolute inset-0 bg-gradient-to-t from-black/80 to-transparent opacity-0 group-hover:opacity-100 h-full transition-opacity flex items-end justify-between p-4'>
+                    {projectGroup.github && (
+                      <a href={projectGroup.github} target="_blank" rel="noopener noreferrer">
+                        <Github className='hover:text-white hover:bg-amber-700 hover:rounded-1xl pointer-events-auto w-8 h-8' />
+                      </a>
+                    )}
+                    {projectGroup.linkProject && (
+                       <a href={projectGroup.linkProject} target="_blank" rel="noopener noreferrer">
+                        <SquareArrowOutUpRight className='hover:text-white hover:bg-amber-700 hover:rounded-1xl pointer-events-auto w-8 h-8' />
+                      </a>
+                    )}
                   </div>
-                  <img src={projectGroup.image} alt="imagen del proyecto" className='' />
+                  <img
+                    src={projectGroup.image}
+                    alt="imagen del proyecto"
+                    className='object-cover w-full h-full'
+                  />
                 </div>
 
                 <div className='grid gap-1'>
                   <div className=' p-2'>
-                    <h3 className='text-xl font-bold mb-2 group-hover:text-orange-500 transition-colors'>{projectGroup.projectName}</h3>
+
+                    <h3 className='text-xl font-bold mb-2 group-hover:text-orange-500 transition-colors '>{projectGroup.projectName}</h3>
                     <p className='text-gray-400 text-opacity-70'> {projectGroup.decription}</p>
+
+                    {/* Status */}
+                    {projectGroup.status ? (
+                      <div className="flex items-center gap-2">
+                      <div className="flex items-center gap-2 bg-green-500/10 border border-green-500/20 rounded-full px-3 py-0.5">
+                        <div className="w-2 h-2 bg-green-400 rounded-full animate-pulse" />
+                        <span className="text-green-400 text-sm font-medium">In progress</span>
+                      </div>
+                    </div>
+                    ) : (
+                      <div className="flex items-center gap-2">
+                      <div className="flex items-center gap-2 bg-red-500/10 border border-red-500/20 rounded-full px-3 py-0.5">
+                        <div className="w-2 h-2 bg-red-400 rounded-full" />
+                        <span className="text-red-400 text-sm font-medium">Backlog</span>
+                      </div>
+                    </div>
+                    ) }
+                    
+
                   </div>
                   <div className=''>
-                    {/* {projectGroup.badge.map((filtrar, filterIndex) => (
+                    {projectGroup.badge.map((filtrar, filterIndex) => (
                       <Badge key={filterIndex} className='className="bg-orange-950/30 text-orange-500 border-orange-800 hover:bg-orange-950/50 m-2 text-xs p-1 border inline-block rounded-full px-3 '>
                         {filtrar}
                       </Badge>
-                    ))} */}
+                    ))}
                   </div>
                 </div>
               </div>
