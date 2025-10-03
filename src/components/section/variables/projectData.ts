51cbc783eaf739e40b0565a151977ab7e5ad8ee4
SRTENEBROSO00 <ccastro@cincinnatus.edu.do>
2025-10-03 11:59:37 -0400

    fix: fixing buttons on responsive

---
+++ src/components/section/variables/projectData.ts
@@ -0,0 +1,32 @@
+import Image from "../../../assets/noImageYet.webp"
+import Mikens from "../../../../public/systemImagenMikens.png"
+ 
+ export const projectData = [{
+    projectName: 'Mikens',
+    image: Mikens,
+    status: true,
+    github: "https://github.com/jeremyda173/sistem-fact-2",
+    linkProject: "https://sistem-fact-2.vercel.app/",
+    decription: "System to build systems",
+    badge: ["React", "Chakra", "Next.js", "VSCode"]
+  },
+  {
+    projectName: 'Building one...',
+    image: Image,
+    status: false,
+    github: "",
+    linkProject: "",
+    decription: "I'll add all my future new projects soon",
+    badge: []
+  },
+  {
+    projectName: 'Building one...',
+    image: Image,
+    status: false,
+    github: "",
+    linkProject: "",
+    decription: "I'll add all my future new projects soon",
+    badge: []
+
+  }
+  ]
